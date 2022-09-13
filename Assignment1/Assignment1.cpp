f4c36da2f6929bc117eae3eb6ad3efb520e2dadd
Andrew <2andrewed@gmail.com>
2022-09-13 00:49:08 -0400

    Refactor

--- Assignment1/Assignment1.cpp
+++ Assignment1/Assignment1.cpp
@@ -9,32 +9,81 @@
 #include <vector>
 #include <cmath>
 
-// Namespace 
-using namespace std;
-using namespace cv;
+std::vector<cv::String> readFiles(std::string filepath, bool yes_sub_directory);
+std::map<int, int> grayIntensity(cv::Mat img);
+std::vector<std::map<int, int>> colorIntensity(cv::Mat img);
+std::map<int, double> pdIntensity(cv::Mat img, std::map<int, int> iv);
+double pd_check(std::map<int, double> pd);
+double calcMean(std::map<int, double> pd);
+double calcVariance(std::map<int, double> pd, int mean);
+double calcCentralMoments(std::map<int, double> pd, int mean, int n);
 
 int main()
 {
-    
-    // Read an image
-    Mat img = imread("test.jpg");
-    //Mat img = imread("test2.jpg");
-    //Mat img = imread("test3.png");
-    map<int, int> iv;
-    map<int, double> pd;
-    vector<double> vec;
-    int possibleIntensity = img.rows * img.cols;
-    
-    // Display the image
-    imshow("Original image", img);
+    // TODO: Allow user to select path, n value. Then gray/color will be determined by name, fix up logic for color vs gray 
 
-    // Populate map with intensity values used, along with its occurrences in the image
+    std::vector<cv::String> list_of_imgs = readFiles("D:/Coding Projects/Assignment1/Assignment1/Images", true);
+
+    if (list_of_imgs.size() == 0) {
+        std::cout << "ERROR: Directory does not exist or is empty" << std::endl;
+    }
+
+    for (int i = 0; i < list_of_imgs.size(); i++) {
+        bool isColor = false;
+        bool isGray = false;
+
+        std::cout << "Reading Image: " << list_of_imgs[i] << std::endl;
+
+        // Read an image
+        cv::Mat img = cv::imread(list_of_imgs[i]);
+  
+        std::string filepath = list_of_imgs[i];
+        cv::imshow(filepath, img);
+
+        std::map<int, int> iv = grayIntensity(img);
+        std::map<int, double> pd = pdIntensity(img, iv);
+
+        double probabilityCheck = pd_check(pd);
+        double mean = calcMean(pd);
+        double variance = calcVariance(pd, mean);
+        double centralMoments = calcCentralMoments(pd, mean, 2);
+
+        std::cout << "Sum of Probability Distrubition: " << probabilityCheck << std::endl;
+        std::cout << "Mean: " << mean << std::endl;
+        std::cout << "Variance: " << (int)variance << std::endl;
+        std::cout << "Central Moments: " << (int)centralMoments << std::endl;
+    }
+        // Wait for a keystroke
+        cv::waitKey(0);
+}
+
+// Reads files in a directory
+std::vector<cv::String> readFiles(std::string filepath, bool yes_sub_directory)
+{
+    std::string folder(filepath);
+    std::vector<cv::String> filenames;
+    
+    cv::glob(folder, filenames, yes_sub_directory);
+
+    for (int i = 0; i < filenames.size(); i++)
+    {
+        std::cout << "Image Found..." << filenames[i] << std::endl;
+    }
+
+    return filenames;
+}
+
+// Populate map with intensity values used, along with its occurrences in the grayscale image
+std::map<int, int> grayIntensity(cv::Mat img)
+{
+    std::map<int, int> iv;
+
     for (int i = 0; i < img.rows; i++) {
         for (int j = 0; j < img.cols; j++) {
             int intensity = (int)img.at<uchar>(i, j);
             if (iv.count(intensity) > 0) {
                 int newKey = iv.find(intensity)->second + 1;
-                std::map<int,int>::iterator iter = iv.find(intensity);
+                std::map<int, int>::iterator iter = iv.find(intensity);
                 if (iter != iv.end()) {
                     iter->second = newKey;
                 }
@@ -44,54 +93,114 @@ int main()
             }
         }
     }
+    return iv;
+}
 
-    // Prints Probability distribution for every intensity
+// Populate map with intensity values used, along with its occurrences in the color image
+std::vector<std::map<int, int>> colorIntensity(cv::Mat img)
+{
+    std::map<int, int> red_iv;
+    std::map<int, int> blue_iv;
+    std::map<int, int> green_iv;
+
+    for (int i = 0; i < img.rows; i++) {
+        for (int j = 0; j < img.cols; j++) {
+
+            int red = img.at<cv::Vec3b>(i, j)[0];
+            int blue = img.at<cv::Vec3b>(i, j)[1];
+            int green = img.at<cv::Vec3b>(i, j)[2];
+
+            // Red
+            if (red_iv.count(red) > 0) {
+                int redKey = red_iv.find(red)->second + 1;
+                std::map<int, int>::iterator iter = red_iv.find(red);
+                if (iter != red_iv.end()) {
+                    iter->second = redKey;
+                }
+            }
+            else {
+                red_iv.insert({ red, 1 });
+            }
+
+            // Blue
+            if (blue_iv.count(blue) > 0) {
+                int blueKey = blue_iv.find(blue)->second + 1;
+                std::map<int, int>::iterator iter = blue_iv.find(blue);
+                if (iter != blue_iv.end()) {
+                    iter->second = blueKey;
+                }
+            }
+            else {
+                blue_iv.insert({ blue, 1 });
+            }
+
+            // Green
+            if (green_iv.count(green) > 0) {
+                int greenKey = green_iv.find(green)->second + 1;
+                std::map<int, int>::iterator iter = green_iv.find(green);
+                if (iter != green_iv.end()) {
+                    iter->second = greenKey;
+                }
+            }
+            else {
+                green_iv.insert({ green, 1 });
+            }
+        }
+    }
+    return { red_iv, blue_iv, green_iv };
+}
+
+// Prints Probability distribution for every intensity
+std::map<int, double> pdIntensity(cv::Mat img, std::map<int, int> iv)
+{
+    int possibleIntensity = img.rows * img.cols;
+    std::map<int, double> pd;
+
     for (auto itr = iv.begin(); itr != iv.end(); ++itr) {
-        double result = itr->second / (double) possibleIntensity;
+        double result = itr->second / (double)possibleIntensity;
         pd.insert({ itr->first, result });
-        vec.push_back(result);
-        cout << "For Intensity " << itr->first << " the probability is: " << result << endl;
+        std::cout << itr->first << " " << result << std::endl;
     }
+    return pd;
+}
 
-    // Checks that the probability distribution  is valid
+// Checks that the probability distribution  is valid
+double pd_check(std::map<int, double> pd)
+{    
     double probabilityCheck = 0;
-    for (int i = 0; i < vec.size(); i++) {
-        probabilityCheck = probabilityCheck + vec[i];
+    for (auto itr = pd.begin(); itr != pd.end(); ++itr) {
+        probabilityCheck += itr->second;
     }
+    return probabilityCheck;    
+}
 
-    cout << "Probability Distrubition Check: " << probabilityCheck << endl;
-    
-    // Computes the mean
+// Computes the mean
+double calcMean(std::map<int, double> pd)
+{
     double mean = 0.0;
     for (auto itr = pd.begin(); itr != pd.end(); ++itr) {
-        // cout << itr->first << " " << itr->second << endl;
         mean += itr->first * itr->second;
     }
-    cout << "Mean: " << mean << endl;
+    return mean;
+}
 
-    // Compute Variance
+// Compute Variance
+double calcVariance(std::map<int, double> pd, int mean)
+{
     double variance = 0.0;
     for (auto itr = pd.begin(); itr != pd.end(); ++itr) {
         variance += pow((mean - itr->first), 2) * itr->second;
-        // cout << "Variance of intensity " << itr->first << " is " << variance << endl;
     }
-    cout << "Variance: " << (int)variance << endl;
+    return variance;
+}
 
-    // Compute Central Moments
-    // TODO: This ain't right apparently
+// Compute Central Moments
+double calcCentralMoments(std::map<int, double> pd, int mean, int n)
+{  
     double centralMoments = 0.0;
     for (auto itr = pd.begin(); itr != pd.end(); ++itr) {
-        centralMoments += pow((mean - itr->first), 3) * itr->second;
-        //cout << "Central Moments of intensity " << itr->first << " is " << centralMoments << endl;
+        centralMoments += pow((mean - itr->first), n) * itr->second;
     }
-    cout << "Central Moments: " << (int)centralMoments << endl;
-    // Equation(3) says, multiply each pixel value with its probability of occurrence p(z_k).p(z_k) computed by using equation 1. Then, sum these products.
-    // Variance(equation 4) is computed as : subtract mean(m) from each intensity value and square it, then multiply with pixel's probability of occurrence p(z_k). 
-    // Equation 5 is similar to equation 4. In equation 4, we square the difference of pixel value and mean. In equation 5, instead of squaring, we raise it to the given n.
-    
-    
-    // Wait for a keystroke
-    waitKey(0);
-
+    return centralMoments;
 }
 
